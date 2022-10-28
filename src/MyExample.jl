859093c3e4669365d5a2b71e6a30b79b8a0b03be
Xiaozheng Ma <xzma2002@yahoo.com>
2022-10-28 16:19:08 -0400

    add the my_f function
    my_f is a linear function

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,9 @@
 module MyExample
 
 # Write your package code here.
+greet() = print("Hello World!")
 
+include("extra_file.jl")
+
+export my_f
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "MyExample.jl" begin
     # Write your tests here.
+    @test my_f(2,1) == 5
+    @test my_f(2, 3) == 7
 end
